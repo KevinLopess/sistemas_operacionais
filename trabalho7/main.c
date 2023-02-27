a8108cc1e4c628a5ed68176f3d9aa43f170defdf
KevinLopess <48494083+KevinLopess@users.noreply.github.com>
2023-02-27 09:46:37 -0300

    T7 - Jantar de Gauleses - Passagem de mensagem

---
+++ trabalho7/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+//#include <mqueue.h>
+#include <sys/fcntl.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+	pid_t pid;
+
+    pid = fork();
+	if(pid > 0)
+	{
+		system("gcc Produtor.c -o prod -lrt");
+		execl("./prod","", NULL);
+	} else if (pid == 0) {
+
+		sleep(1);
+		system("gcc Consumidor.c -o cons -lpthread -lrt ");
+		execl("./cons", "", NULL);
+	} else {
+		printf("Erro\n");
+	}
+
+	exit(0);
+}
